ca0582044b4a85353c97849e2a4732834ffa6ba9
Andreas Kater <andreas.kater@gmail.com>
2019-02-17 01:15:26 +0100

    added git_merge_pull_request.sh

---
+++ scripts/git_merge_pull_request.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -e
+
+feature_branch=`git symbolic-ref HEAD`
+feature_branch=${feature_branch##refs/heads/}
+target_branch='master'
+verbose='false'
+
+while getopts 'b:' flag; do
+  case "${flag}" in
+    b) target_branch=${OPTARG} ;;
+    v) verbose='true' ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
+
+if [ "$feature_branch" == $target_branch ]; then
+  echo "You're on $target_branch branch, checkout feature
+  branch you want to merge"
+  exit 1
+fi
+
+echo "git fetch origin"
+git fetch origin
+
+echo "git rebase origin/${target_branch}"
+git rebase origin/$target_branch
+
+echo "git push -f"
+git push -f
+
+echo "git checkout ${target_branch}"
+git checkout $target_branch
+
+echo "git merge $feature_branch"
+git merge $feature_branch
+
+echo "git push"
+git push
+
+echo "git push origin --delete $feature_branch"
+git push origin --delete $feature_branch
+
+echo "git branch -d $feature_branch"
+git branch -d $feature_branch
+
+echo "$feature_branch has been merged into
+${target_branch}. All is well!"
+
