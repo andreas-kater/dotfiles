29d45543c98524f73d3847d2a2ad19918a54b9ab
Andreas Kater <andreas.kater@gmail.com>
2021-05-12 09:43:55 +0100

    added gcmpw

--- scripts/gcmpw.js
+++ scripts/gcmpw.js
@@ -29,8 +29,23 @@ const gcmp = async ()=>{
     await run(`git commit -m "${process.argv[2]}"`)
     await run(`git push`)
     await run(`git --no-pager log --oneline --decorate --graph --all -n 12`)
+
+    await new Promise(resolve=>setTimeout(()=>resolve(),3000))
+
+    const gh_list = await run('gh run list -L1')
+    if(gh_list){
+      console.log(gh_list)
+      const regex = /([0-9]{9})/g
+      const match = regex.exec(gh_list)
+      if(match){
+        const run_id = match[1] 
+        await run(`gh run watch ${run_id} -i3`)
+      }
+    }
+    await run(`terminal-notifier -title 'Github' -message 'Done deploying'`)
+    // await run(`say 'done deploying'`)
   }catch(err){
-    await run('say "commit failed"')
+    await run('say "deploy failed"')
     console.log(err)
   }
 }
